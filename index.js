062169ffe3e1e6c77147c4eefedfc2c330298505
Md Ashikur Rahman <86632525+iamAshiq1@users.noreply.github.com>
2022-11-19 01:53:40 +0600

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.obi7m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://Ashik:90875@cluster0.obi7m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
